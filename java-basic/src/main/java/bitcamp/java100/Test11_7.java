85649feeacc298ef7885a95e7f3a6ebd3dcf8f2c
angelpjn <angelpjn@naver.com>
2017-09-25 19:11:28 +0900

    'm'

---
+++ java-basic/src/main/java/bitcamp/java100/Test11_7.java
@@ -0,0 +1,18 @@
+
+package bitcamp.java100;
+
+public class Test11_7 {
+
+    public static void main(String[] args){
+
+        System.out.println(-21_4748_3648);
+        System.out.println(21_4748_3647);
+
+        System.out.println(100);
+        System.out.println(Integer.toBinaryString(100));
+
+        System.out.println(-100);
+        System.out.println(Integer.toBinaryString(-100));
+
+    }
+}
