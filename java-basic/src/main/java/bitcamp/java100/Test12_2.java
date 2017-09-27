15979854b81d0629a0bd38a5a9ff7f8421e19e53
angelpjn <angelpjn@naver.com>
2017-09-27 19:19:50 +0900

    'd

---
+++ java-basic/src/main/java/bitcamp/java100/Test12_2.java
@@ -0,0 +1,20 @@
+
+package bitcamp.java100;
+
+public class Test12_2 {
+
+    public static void main(String[] args){
+       
+        int i;
+        int j;
+
+        i = 100;
+        j = i;
+        // 100 = 200;
+        System.out.println(i);
+        System.out.println(j);
+
+
+
+    }
+}
---
+++ java-basic/src/main/java/bitcamp/java100/Test12_3.java
@@ -0,0 +1,25 @@
+
+package bitcamp.java100;
+
+public class Test12_3 {
+
+    public static void main(String[] args){
+       
+        int i, j;
+        i = 100;
+        j = 200;
+
+        int k = 100;
+
+        int x = 100, y, z = 300;
+
+        System.out.println(i);
+        System.out.println(j);
+        System.out.println(k);
+        System.out.println(x);
+        System.out.println(z);
+
+        // System.out.println(y); // 초기화 미수행으로 인한 컴파일 오류
+
+    }
+}
