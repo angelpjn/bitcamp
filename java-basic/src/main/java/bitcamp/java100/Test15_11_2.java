57bac55f68550ea12bfb566aaa8f36e50bf36e42
Jungnam Park <angelpjn@naver.com>
2017-10-11 12:25:48 +0900

    ..

--- java-basic/src/main/java/bitcamp/java100/Test15_11_2.java
+++ java-basic/src/main/java/bitcamp/java100/Test15_11_2.java
@@ -2,50 +2,39 @@
 package bitcamp.java100;
 
 /*
-파일 정보 저장_비트 연산자 미사용
+파일 정보 저장_배열을 이용하여 처리(메모리 절약, 가독성 떨어짐)
 - 저장 대상 : 디렉토리여부, 소유주, 그룹, 기타 사용자의 사용 권한(읽기, 쓰기, 실행) 정보
 */
 
   
-//아ㅏㅏㅏㅏ
 public class Test15_11_2 {
     public static void main(String[] args){
-    	class Privilege {
-    		boolean isDirectory;
-    		boolean ownerRead;
-    		boolean ownerWrite;
-    		boolean ownerExe;
-    		boolean groupRead;
-    		boolean groupWrite;
-    		boolean groupExe;
-    		boolean otherRead;
-    		boolean otherWrite;
-    		boolean otherExe;
-    	}
+
+    	boolean [] arr = new boolean[10];
     	
-    	Privilege p1 = new Privilege();
-    	
-		p1.isDirectory = false;
-		p1.ownerRead = true;
-		p1.ownerWrite = true;
-		p1.ownerExe = true;
-		p1.groupRead = true;
-		p1.groupWrite = false;
-		p1.groupExe = true;
-		p1.otherRead = true;
-		p1.otherWrite = false;
-		p1.otherExe = true;
+		arr[0] = false;
+		arr[1] = true;
+		arr[2] = true;
+		arr[3] = true;
+		arr[4] = true;
+		arr[5] = true;
+		arr[6] = false;
+		arr[7] = true;
+		arr[8] = true;
+		arr[9] = false;
+
 		
-		System.out.print(p1.isDirectory ? "d" : "-");
-		System.out.print(p1.ownerRead ? "r" : "-");
-		System.out.print(p1.ownerWrite ? "w" : "-");
-		System.out.print(p1.ownerExe ? "x" : "-");
-		System.out.print(p1.groupRead ? "r" : "-");
-		System.out.print(p1.groupWrite ? "w" : "-");
-		System.out.print(p1.groupExe ? "x" : "-");
-		System.out.print(p1.otherRead ? "r" : "-");
-		System.out.print(p1.otherWrite ? "w" : "-");
-		System.out.print(p1.otherExe ? "x" : "-");
+		
+		System.out.print(arr[0] ? "d" : "-");
+		System.out.print(arr[1] ? "r" : "-");
+		System.out.print(arr[2] ? "w" : "-");
+		System.out.print(arr[3] ? "x" : "-");
+		System.out.print(arr[4] ? "r" : "-");
+		System.out.print(arr[5] ? "w" : "-");
+		System.out.print(arr[6] ? "x" : "-");
+		System.out.print(arr[7] ? "r" : "-");
+		System.out.print(arr[8] ? "w" : "-");
+		System.out.print(arr[9] ? "x" : "-");
 		System.out.println();
     }
 }
