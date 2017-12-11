79b7d9d0dcc5267084024bb7efb115d9aabd0ab1
angelpjn <angelpjn@naver.com>
2017-12-11 18:29:28 +0900

    'g'

---
+++ java-web/src/main/java/step8/Servlet06Test.java
@@ -0,0 +1,87 @@
+package step8;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.Socket;
+
+public class Servlet06Test {
+    public static void main(String[] args) throws Exception {
+        try (
+             Socket socket = new Socket("localhost", 9999);
+             PrintStream out = new PrintStream(socket.getOutputStream());
+             BufferedReader in = new BufferedReader(
+                     new InputStreamReader(socket.getInputStream()));) {
+
+             out.println("GET /java-web/step8/Servlet06 HTTP/1.1");
+             out.println("Host: localhost");
+             out.println();
+
+             String str = null;
+             while ((str = in.readLine()) != null) {
+                System.out.println(str);
+            }
+        
+        } catch (Exception e) {}
+
+        System.out.println("----------------------------------------------------");
+        
+        try (
+                Socket socket = new Socket("localhost", 9999);
+                PrintStream out = new PrintStream(socket.getOutputStream());
+                BufferedReader in = new BufferedReader(
+                        new InputStreamReader(socket.getInputStream()));) {
+            
+            out.println("HEAD /java-web/step8/Servlet06 HTTP/1.1");
+            out.println("Host: localhost");
+            out.println();
+            
+            String str = null;
+            while ((str = in.readLine()) != null) {
+                System.out.println(str);
+            }
+            
+        } catch (Exception e) {}
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
